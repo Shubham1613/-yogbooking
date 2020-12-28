151a6b98bdba38539ad0fb501f94e891ef04ed41
gourav <GouravKumarSharma>
2020-12-28 14:26:41 +0530

    ver2.1

--- yogbooking/yogbookingapp/views.py
+++ yogbooking/yogbookingapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from .models import Services
 # Create your views here.
 def index(request):
 
     return render(request, 'yogbookingapp/index.html')
 def services(request):
-    return render(request,'yogbookingapp/services.html')
+    services = Services.objects.all()
+
+    return render(request,'yogbookingapp/services.html',{'services' : services})
 def events(request):
     return render(request,'yogbookingapp/events.html')
